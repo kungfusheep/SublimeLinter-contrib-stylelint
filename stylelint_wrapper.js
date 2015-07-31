9ba40eae5c8710e6cc209a4480e21c4431d42011
Pete Griffiths <kungfusheep@users.noreply.github.com>
2015-07-31 12:23:41 +0100

    Update stylelint_wrapper.js

--- stylelint_wrapper.js
+++ stylelint_wrapper.js
@@ -4,18 +4,21 @@ var postcss = require("postcss")
 var stylelint = require("stylelint")
 var reporter = require("./reporter")
 
-// css to be processed
+/// css to be processed
 var fileName = process.argv[2];
 var css = fs.readFileSync(fileName, "utf8");
+
+/// load the .stylelintrc config, if --config has been provided.
 var config;
 var index = process.argv.indexOf("--config");
 if(index > -1){
 	config = JSON.parse( fs.readFileSync(process.argv[index+1]) );
 }
 
+/// start the linting process.
 postcss([
-        stylelint( config ? config : undefined )
-		,reporter()
+        stylelint( config )
+	,reporter()
     ])
     .process(css, {
         from: fileName
